f639bfdb82869dad2c3fc920cd1b5c1483511749
yair681 <przyyryair@gmail.com>
2026-03-13 09:13:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ function startClient() {
     client = new Client({
         authStrategy: new LocalAuth(),
         puppeteer: {
+            executablePath: '/opt/render/.cache/puppeteer/chrome/linux-146.0.7680.66/chrome-linux64/chrome',
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
